6c322fdc206ae00b76e10ccb0bafb5700e429859
TheSwaroop <raparthiswaroop6143@gmail.com>
2025-05-15 21:50:30 +0530

    Add files via upload

---
+++ BlowfishEncryption.java
@@ -0,0 +1,40 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class BlowfishEncryption {
+    public static void main(String[] args) {
+        try {
+            // Generate Blowfish key
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
+            keyGenerator.init(128);
+            SecretKey secretKey = keyGenerator.generateKey();
+
+            // Create Blowfish cipher instance
+            Cipher cipher = Cipher.getInstance("Blowfish");
+
+            // Read plaintext input
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter plaintext: ");
+            String plaintext = scanner.nextLine();
+            scanner.close();
+
+            // Encrypt the plaintext
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
+            String encryptedText = Base64.getEncoder().encodeToString(encryptedBytes);
+            System.out.println("Encrypted: " + encryptedText);
+
+            // Decrypt the ciphertext
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+            String decryptedText = new String(decryptedBytes);
+            System.out.println("Decrypted: " + decryptedText);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
